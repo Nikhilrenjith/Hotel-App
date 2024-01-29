169a498250a5bc928723b16cf5df9a533f9a3e40
Nikhilrenjith <42229967+Nikhilrenjith@users.noreply.github.com>
2024-01-29 17:07:40 +0530

    Update All Locations.js

--- frontend/src/components/Locations/All Locations.js
+++ frontend/src/components/Locations/All Locations.js
@@ -8,7 +8,7 @@ const Locations = () => {
   const [dataList, setDataList] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3001/images")
+    fetch("https://hotel-app-api-green.vercel.app/images")
       .then((response) => response.json())
       .then((data) => setDataList(data))
       .catch((error) => console.error(error));
